e9ae8278c5bd2c450d3aadd0feeda71675b6580d
Olena Piankova <el.piankova@gmail.com>
2020-02-13 19:25:53 +0200

    PageObject continue

--- test_login.py
+++ test_login.py
@@ -7,6 +7,7 @@ def test_login(driver):
 
     main_page = MainPage(driver)
     sign_in_page = main_page.sign_in_click()
+    # sign_in_page.username_field.send_keys(user)
     sign_in_page.fill_form(user, password)
     dashboard_page = sign_in_page.submit()
     assert dashboard_page.is_this_page()
--- test_status_create.py
+++ test_status_create.py
@@ -1,9 +1,21 @@
-def test_status_create(logged_user, app):
-    old_posts = app.get_posts()
-    app.create_post("Great day!!!")
-    app.wait_new_post(len(old_posts))
-    new_post = app.get_posts()[0]
-    assert new_post.text == "Great day!!!"
+from pages.internal_pages import DashboardPage
+import pytest
+
+
+def test_post_create(logged_user, driver):
+    input_text = "Great day!"
+    dashboard_page = DashboardPage(driver)
+    old_posts = dashboard_page.posts
+    dashboard_page.create_post(input_text)
+    dashboard_page.wait_new_post(len(old_posts))
+    new_post = dashboard_page.posts[0]
+    assert input_text in new_post.text
+
+    # old_posts = app.get_posts()
+    # app.create_post("Great day!!!")
+    # app.wait_new_post(len(old_posts))
+    # new_post = app.get_posts()[0]
+    # assert new_post.text == "Great day!!!"
     # assert new_post.author == logged_user
     # assert new_post.time == "within 1 min"
 
